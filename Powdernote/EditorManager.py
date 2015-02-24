7a8ce08227d5a3361467167053a204d2f78a1875
Gabriel Ankeshian <anke@zhaw.ch>
2015-02-24 12:03:00 +0100

    T859
    Test Plan: review by piiv
    
    Reviewers: piiv
    
    Reviewed By: piiv
    
    Maniphest Tasks: T859
    
    Differential Revision: http://erlang.cloudcomplab.ch/D35

--- Powdernote/EditorManager.py
+++ Powdernote/EditorManager.py
@@ -111,7 +111,8 @@ class EditorManager(object):
             editor = 'E'
         else:
             editor = 'vi'
-        return (os.environ.get("VISUAL") or
+        return (os.environ.get("PDRNOTE_EDITOR ") or
+                os.environ.get("VISUAL") or
                 os.environ.get("EDITOR", editor))
 
     def _openEditor(self, filename):
